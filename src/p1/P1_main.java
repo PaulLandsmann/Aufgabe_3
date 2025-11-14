65846534b36cba22bee9d0fa093af0961060b85e
PaulLandsmann <p.landsmann@online-home.de>
2025-11-14 18:58:06 +0100

    p1 fertig

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -2,9 +2,26 @@ package p1;
 
 public class P1_main {
 
+	
 	public static void main(String[] args) {
+		int wuerfel = java.util.concurrent.ThreadLocalRandom.current().nextInt(1, 7);
 		
-
+		if(wuerfel == 1 || wuerfel == 4) {
+			System.out.print("Gewonnen!");
+			
+		}
+		
+		if(wuerfel == 2) {
+			System.out.print("Verloren!");
+		}
+		
+		if(wuerfel == 5) {
+			System.out.print("Unentschieden!");
+		}
+		
+		if(wuerfel == 3 || wuerfel == 6) {
+			System.out.print("Unklar!");
+		}
 	}
 
 }
