1243b5902858df8cd87337849dcf6a372adfede1
PaulLandsmann <p.landsmann@online-home.de>
2025-11-17 20:30:47 +0100

    p3 fertig woche 3

--- src/p3/P3_main.java
+++ src/p3/P3_main.java
@@ -3,8 +3,17 @@ package p3;
 public class P3_main {
 
 	public static void main(String[] args) {
+		int zahl = -71;
+		System.out.println(Math.abs(zahl));
+
+		System.out.println(zahl * zahl);
+		System.out.println(Math.pow(zahl, 2));
 		
-
+		if(zahl %2 == 0) {
+			System.out.println("Zahl ist gerade");
+		}
+		else {
+			System.out.println("Zahl ist ungerade");
+		}
 	}
-
 }
